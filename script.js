f7c3ea43d6194a472b7502f0ac02090915ade4ac
sagesaka <sagesaka@riprice.co.jp>
2024-09-04 14:24:44 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const getGitUsername = async () => {
+  const message = 'GitのユーザーIDは';
+  const url = 'https://api.github.com/users/sagesaka'
+
+  const json = await fetch(url)
+      .then(res => {
+          console.log('これは非同期処理成功時のメッセージです')
+          return res.json()
+      }).catch(error => {
+          console.error('これは非同期処理失敗時のメッセージです。', error)
+          return null
+      });
+
+  const username = json.login;
+  console.log(message + username)
+}
+
+getGitUsername()
